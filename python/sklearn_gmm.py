4eab14363153ae82367a92534296661bc8b591a7
Alphajun <jiajunwan2015@gmail.com>
2021-12-06 21:47:54 -0500

    Update sklearn_gmm.py

--- python/sklearn_gmm.py
+++ python/sklearn_gmm.py
@@ -6,11 +6,13 @@ def _estimate_log_gaussian_prob(X, means, precisions_chol, covariance_type = "sp
     log_det = n_features * (np.log(precisions_chol))
     precisions = precisions_chol ** 2
     start_time = time.time()
-    test = np.einsum("ij,ij->i", X, X)
+    kernel_1 = np.dot(X, means.T * precisions)
+    precisions *= -0.5
+    kernel_2 = np.einsum("ij,ij->i", X, X)
+    kernel_3 = np.outer(kernel_2, precisions)
+    a = kernel_1
+    b = kernel_3
+    c = np.sum(means ** 2, 1) * precisions + -0.5 * n_features * np.log(2 * np.pi) + log_det
+    kernel_4 = a + b + c
     end_time = time.time() - start_time
-    log_prob = (
-        np.sum(means ** 2, 1) * precisions
-        - 2 * np.dot(X, means.T * precisions)
-        + np.outer(test, precisions)
-    )
-    return -0.5 * (n_features * np.log(2 * np.pi) + log_prob) + log_det, end_time
+    return kernel_4, end_time
